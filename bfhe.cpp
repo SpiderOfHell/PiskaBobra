81fa0bb2ce199898e67742dbd3a3b85b27c56fc3
SpiderOfHell <1982galka@gmail.com>
2025-10-03 11:06:11 +0300

    V1.2. minor changes for конфигурация этап

--- bfhe.cpp
+++ bfhe.cpp
@@ -2,19 +2,22 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <filesystem>
+#include <fstream>
+#include <windows.h>;
 using namespace std;
-enum commands
+enum commands //энум
 {
-	ERROR = -1,
+	_ERROR = -1,
 	EXIT = 1,
 	CD = 2,
 	HELP = 3,
 	LS = 4,
 	TOCHKATOCHKA = 5,
-	SLESH = 6
-
+	SLESH = 6,
+	RUN = 7
 };
-int getCommandCode(const string& command)
+int getCommandCode(const string& command)//костыльное для свичей
 {
 	if (command == "cd")
 		return CD;
@@ -28,77 +31,125 @@ int getCommandCode(const string& command)
 		return TOCHKATOCHKA;
 	if (command[0] == '/')
 		return SLESH;
-	return	ERROR;
+	if (command == "run")
+		return RUN;
+	return _ERROR;
 }
-/*class cmdpath
+void executeScript(const string& path, const string& name)//обработка файла
 {
-	string path;
-	string* father;
-	vector<string*>children;
-public:
-	cmdpath(string*father, string path, vector<string*> newchildren)
+	ifstream f(path);
+	if (!f.is_open())
 	{
-		this->father = father;
-		this->path = path;
-		for (auto p : newchildren)
+		cout << "Ошибка открытия скрипта: " << path << endl;
+		return;
+	}
+	string line;
+	while (getline(f, line))//построчное чтение
+	{
+		string comment;
+		size_t pos = line.find("//");//нахождение в строке комментария
+		if (pos != string::npos)//если существует комментарий
 		{
-			children.push_back(p);
+			comment = line.substr(pos + 2);//чтение коммментария с удалением слешей
+			line = line.substr(0, pos);//удаление комментария из строки
+		}
+		if (line.empty() && comment.empty())//если ничего нет, то скоп итерации
+			continue;
+		cout << name << ">" << line;
+		if (!comment.empty())//если комментарий был задан, то выводится на экран после комманды
+			cout << " //" << comment;
+		cout << endl;
+		istringstream input(line);//парсер аргументов
+		vector<string> parts;
+		string token;
+		while (input >> token)
+			parts.push_back(token);
+		if (parts.empty())//если аргументов не было, но был комментарий
+		{
+			if (!comment.empty())
+				cout << "Комментарий: " << comment << endl;
+			continue;
+		}//не забыть заменить код на костыль при создании обработки аргументов
+		int code = getCommandCode(parts[0]);//тут без костыля, так как он изначально был немного для другого
+		parts.erase(parts.begin());//удаление кода комманды, чтобы не вызывалась повторно при будущем добавлении логики обработки
+		switch (code)
+		{
+		case CD:
+			cout << name << ">Вызвана команда cd с аргументами:\n";
+			for (auto& p : parts) cout << "\t" << p << "\n";
+			if (!comment.empty())
+				cout << "Комментарий: " << comment << "\n";
+			cout << "Всего аргументов было: " << parts.size() << "\n";
+			break;
+		case LS:
+			cout << name << ">Вызвана команда ls с аргументами:\n";
+			for (auto& p : parts) cout << "\t" << p << "\n";
+			if (!comment.empty())
+				cout << "Комментарий: " << comment << "\n";
+			cout << "Всего аргументов было: " << parts.size() << "\n";
+			break;
+		case HELP:
+			cout << "Спасение утопающих - дело рук самих утопающих\n";
+			if (!comment.empty())
+				cout << "Комментарий: " << comment << "\n";
+			break;
+		case EXIT:
+			exit(0);
+		case RUN:
+			if (parts.empty())
+			{
+				cout << "Ошибка: не указан путь к скрипту\n";
+				break;
+			}
+			executeScript(parts[0], name);
+			break;
+		default:
+			cout << "Ошибка! комманда не найдена!\n";
+			if (!comment.empty())
+				cout << "Комментарий: " << comment << "\n";
+			break;
 		}
 	}
-	string getpath()
-	{
-		return path;
-	}
-	string* getfather()
-	{
-		return father;
-	}
-	void setpath(string newpath)
-	{
-		path = newpath;
-	}
-	bool setfather(string* newfather)
-	{
-		if(father!=nullptr)
-		{
-			father = newfather;
-			return true;
-		}
-		return false;
-	}
-	void addchild(string*& child)
-	{
-		if (children[0] == nullptr)
-		{
-			children[0]->erase();
-		}
-		children.push_back(child);
-	}
-	string* getchild(int i)
-	{
-		if(!children.empty() && i<children.size())
-			return children[i];
-		return nullptr;
-	}
-	int sizechildren()
-	{
-		return children.size();
-	}
-};*/
-int main()
+}
+int main(int argc, char* argv[])//мэйн
 {
 	setlocale(0, "");
-	/*string* kdw=nullptr;
-	string pat = "ejje/fefe";
-	vector<string*>vec;
-	vec.push_back(kdw);
-	string ko = "fefe";
-	string* jh = &ko;
-	vec.push_back(jh);
-	cmdpath test1(kdw,pat,vec);*/
-	string command, name = "Persona";
+	const string absPath = filesystem::current_path().string() + "\\bfhe.exe";//костыль абсолютного путя к файлу проги
+	const string absScriptPath = filesystem::current_path().string() + "\\start.txt";//аналогично для стартового скрипта
+	if (argc < 3)//при неподаче всех нужных аргументов будет утешительный приз
+	{
+		cout << "Больше аргументов дай лэээээ\n";
+		return 1;
+	}
+	else if (argc > 3)
+	{
+		cout << "Меньше аргументов дай лээээ\n";
+		return 1;
+	}
+	string vfsPath = argv[1];
+	string scriptPath = argv[2];
+	cout << "Отладочный вывод параметров:\n";
+	cout << "Путь к VFS: " << vfsPath << "\n";
+	cout << "Путь к стартовому скрипту: " << scriptPath << "\n";
+	if (vfsPath != absPath)//проверка путя к экзешнику
+	{
+		cout << "Ошибка доступа к файлу VFS. Прекращение работы.\n";
+		return 1;
+	}
+	if (scriptPath != absScriptPath)//проверка путя к скрипту
+	{
+		cout << "Неверный путь к стартовому скрипту\n";
+		return 1;
+	}
+	if (!filesystem::exists(scriptPath))//на случай проблемной прокладки между монитором и креслом
+	{
+		cout << "Верни обратно файл стартового скрипта, его удалять нельзя было\n";
+		return 1;
+	}
+	executeScript(scriptPath, "Persona");//запуск функции обработки стартового скрипта
 	vector<string> line;
 	vector<int> kostil;
+	string command, name = "Persona";
 	cout << "Для вывода списка комманд введите help\n";
 	while (true)
 	{
@@ -131,47 +182,6 @@ int main()
 					cout << "\t" << p << "\n";
 				}
 				cout << "Всего аргументов было: " << line.size() << "\n";
-				/*for (auto p : line)
-				{
-					kostil.push_back(getCommandCode(p));
-				}
-				if (kostil.size() == 0)
-				{
-					cout << "Аргументы:\n/home/user – переход в директорию /home/user\n"
-						<< "..– переход на уровень выше в иерархии директорий\n"
-						<< "~ – переход в домашнюю директорию текущего пользователя\n"
-						<< "- – переход в предыдущую рабочую директорию\n";
-					break;
-				}
-				for(int i=0;i<kostil.size();i++)
-				{
-					switch (kostil[i])
-					{
-					case SLESH:
-						if (kostil.size()!=1)
-						{
-							cout << "Не удалось получить путь, " << line[0]<<" ";
-							for (auto p : line)
-							{
-								if (p == line[0])
-									continue;
-								cout << p << " ";
-							}
-							cout << "не является валидным путём\n";
-							kostil.clear();
-							break;
-						}
-						cout << "какипук\n";
-						break;
-					case TOCHKATOCHKA:
-						cout << "пукикаки\n";
-						break;
-					default:
-						cout << "Ошибка, неизвестный аргумент " << line[i] << "\n";
-						i = kostil.size();
-						break;
-					}
-				}*/
 				break;
 			case EXIT:
 				exit(0);
@@ -187,13 +197,17 @@ int main()
 					cout << "\t" << p << "\n";
 				}
 				cout << "Всего аргументов было: " << line.size() << "\n";
-				/*cout << "Аргументы:\n-a - отображение всего содержимого директории\n"
-					<<"-A - отображение всех файлов, кроме скрытых\n"
-					<<"--author – получение информации об авторе\n"
-					<<"-b – вывод имени файла, даже если в нем содержатся служебные символы, которые не видны при печати.\n";*/
+				break;
+			case RUN:
+				if (line.empty())
+				{
+					cout << "Ошибка: не указан путь к скрипту\n";
+					break;
+				}
+				executeScript(line[0], name);
 				break;
 			default:
-				cout << "Ошибка! комманда не найдена!\nДля вывода списка коммандвведите 'help'\n";
+				cout << "Ошибка! комманда не найдена!\nДля вывода списка комманд введите 'help'\n";
 				break;
 			}
 		}
